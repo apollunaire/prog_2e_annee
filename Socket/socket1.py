2ed3a9c2efccf76d3ff465a83d93b46ddfb54ca2
tiphaineSCHRENCK <tiphaine.schrenck@uha.fr>
2022-11-21 10:23:23 +0100

    TP2

--- Socket/socket1.py
+++ Socket/socket1.py
@@ -1,25 +1,28 @@
 import socket
 
 def socket_client():
-    client_socket = socket.socket() #creation de la socket
-    hostname = socket.gethostname() #recuperation du hostname
-    print(hostname) #affichage hostname
-    host="localhost"
-    port = 10001
-    client_socket.connect((host, port)) #connexion host par le port (si localhost, host="")
-    print("CLIENT")
-    message = input("message a envoyer : ")  # entree du message a envoyer
-    message.lower()
-    while (message!="bye") and (message!="exit"):
-        client_socket.send(message.encode())  # envoi du message
-        data = client_socket.recv(1024).decode()  # reception de donnees
-        print(data)  # affichage des donnees recues
-        message = input("message2 a envoyer : ")  # entree du message a envoyer
+    try :
+        client_socket = socket.socket() #creation de la socket
+        hostname = socket.gethostname() #recuperation du hostname
+        print(hostname) #affichage hostname
+        host="localhost"
+        port = 10001
+        client_socket.connect((host, port)) #connexion host par le port (si localhost, host="")
+        print("CLIENT")
+        message = input("message a envoyer : ")  # entree du message a envoyer
+        message.lower()
+        while (message!="bye") and (message!="exit"):
+            client_socket.send(message.encode())  # envoi du message
+            data = client_socket.recv(1024).decode()  # reception de donnees
+            print(data)  # affichage des donnees recues
+            message = input("message2 a envoyer : ")  # entree du message a envoyer
 
-    client_socket.send(message.encode()) #envoi du message
-    #data = client_socket.recv(1024).decode() #reception de donnees
-    #print(data) #affichage des donnees recues
-    client_socket.close() #fermeture de la connexion / du port
+        client_socket.send(message.encode()) #envoi du message
+        #data = client_socket.recv(1024).decode() #reception de donnees
+        #print(data) #affichage des donnees recues
+        client_socket.close() #fermeture de la connexion / du port
+    except (TimeoutError):
+        return -1
 
 
 (socket_client())
--- Socket/socket2.py
+++ Socket/socket2.py
@@ -2,38 +2,40 @@ import socket
 
 
 def socket_serveur():
-    #udpSvr = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #creation socket en UDP
-    #tcpSvr = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creation socket en TCP
-    server_socket = socket.socket()  #creation de la socket
-    host = "localhost"
-    port = socket.gethostname() #adresse IP ?
-    port = 10001
-    server_socket.bind((host, port))  #association du host et du port
-    server_socket.listen(1)  #attente de la connexion / ici pas de connexions simultanees
-    print(server_socket.listen(1))
-    while True:
-        conn, address = server_socket.accept()  #connexion
-        print(f"SERVEUR")
-        data = conn.recv(1024).decode()  #reception data
-        data.lower()
-        print(data)  #affichage de data
-        #reply = input("message à envoyer : ")  # entree de la reponse
-        while (data != "bye") and (data != "exit"):
-            reply = input("message à envoyer : ")  # entree de la reponse
-            conn.send(reply.encode())  # envoi de la reponse
-
-            data = conn.recv(1024).decode()  # reception data
+    try:
+        #udpSvr = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #creation socket en UDP
+        #tcpSvr = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creation socket en TCP
+        server_socket = socket.socket()  #creation de la socket
+        host = "localhost"
+        port = socket.gethostname() #adresse IP ?
+        port = 10001
+        server_socket.bind((host, port))  #association du host et du port
+        server_socket.listen(1)  #attente de la connexion / ici pas de connexions simultanees
+        print(server_socket.listen(1))
+        while True:
+            conn, address = server_socket.accept()  #connexion
+            print(f"SERVEUR")
+            data = conn.recv(1024).decode()  #reception data
             data.lower()
-            print(data)  # affichage de data
+            print(data)  #affichage de data
+            #reply = input("message à envoyer : ")  # entree de la reponse
+            while (data != "bye") and (data != "exit"):
+                reply = input("message à envoyer : ")  # entree de la reponse
+                conn.send(reply.encode())  # envoi de la reponse
 
-        if data == "bye":
-            conn.connect_ex((host, port))
-        if data == "exit":
-            break
-    conn.close() #fermeture connexion
+                data = conn.recv(1024).decode()  # reception data
+                data.lower()
+                print(data)  # affichage de data
 
-    #server_socket.connect_ex("") #à tester ? // fermeture connexion hote specifie ?
-    #conn.connect_ex(address) #à tester ? // fermeture de la connexion avec l'hote specifie ?
+            if data == "bye":
+                conn.connect_ex((host, port))
+            if data == "exit":
+                break
+        conn.close() #fermeture connexion
+    except (TimeoutError):
+        return -1
+        #server_socket.connect_ex("") #à tester ? // fermeture connexion hote specifie ?
+        #conn.connect_ex(address) #à tester ? // fermeture de la connexion avec l'hote specifie ?
 
 
 (socket_serveur())
